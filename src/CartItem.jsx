7e27f2a604e98df489b7898974645d51ec282fac
Sebastian <sebastian.quinones.inf@gmail.com>
2024-07-26 18:30:38 -0400

    test

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -25,20 +25,35 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+
 
 
   const handleIncrement = (item) => {
+    item.quantity ++;
+    dispatch(updateQuantityAtCart(item));
+
   };
 
   const handleDecrement = (item) => {
-   
+   item.quantity--;
+   if(item.quantity <= 0){
+    dispatch(removeItemFromCart(item));
+   }else{
+    dispatch(updateQuantityAtCart(item));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItemFromCart(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    var cost = Number(cart[i].cost.replace(/[^0-9\.]+/g,""));
+    return item.quantity * cost;
   };
 
   return (
@@ -66,7 +81,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
