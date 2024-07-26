17f8deac5830b4a878ec28406cea924eb7604074
Sebastian <sebastian.quinones.inf@gmail.com>
2024-07-26 17:20:24 -0400

    test

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -256,10 +256,6 @@ const handlePlantsClick = (e) => {
   const handleAddToCart = (product) => {
     debugger;
     dispatch(addItemToCart(product));
-    setAddedToCart((prevState) => ({
-       ...prevState,
-       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
-     }));
   };
 
     return (
@@ -293,7 +289,7 @@ const handlePlantsClick = (e) => {
                         <img className="product-image" src={plant.image} alt={plant.name} />
                         <div className="product-title">{plant.name}</div>
                         {/*Similarly like the above plant.name show other details like description and cost*/}
-                        <button  className="product-button" onClick={() => handleAddToCart(plantIndex)}>Add to Cart</button>
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                     </div>
                     ))}
                 </div>
