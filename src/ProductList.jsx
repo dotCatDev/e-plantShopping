55ee4d30a3a4efbee9d3fd88451ab78ee5b90345
Sebastian <sebastian.quinones.inf@gmail.com>
2024-07-27 00:19:10 -0400

    Final commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -307,7 +307,8 @@ const handlePlantsClick = (e) => {
                         <img className="product-image" src={plant.image} alt={plant.name} />
                         <div className="product-title">{plant.name}</div>
                         {/*Similarly like the above plant.name show other details like description and cost*/}
-                        { addedToCart.hasOwnProperty(plant.name) ? <button className= "product-button btn-disabled" >Added to Cart</button> : addedToCart.hasOwnProperty(plant.name) }<button className= "product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        { addedToCart.hasOwnProperty(plant.name) ? <button className= "product-button btn-disabled" >Added to Cart</button> 
+                        : <button className= "product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button> }
                     </div>
                     ))}
                 </div>
