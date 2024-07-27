311343fea1921d4f9a47f1cd91a35f03fd7ae9b2
Sebastian <sebastian.quinones.inf@gmail.com>
2024-07-26 20:08:08 -0400

    test

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,7 +292,7 @@ const handlePlantsClick = (e) => {
                         <img className="product-image" src={plant.image} alt={plant.name} />
                         <div className="product-title">{plant.name}</div>
                         {/*Similarly like the above plant.name show other details like description and cost*/}
-                        <button className={  setAddedToCart ? "product-button disabled" : "product-button" } onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        <button className={  setAddedToCart.product.name === plant.name ? "product-button disabled" : "product-button" } onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                     </div>
                     ))}
                 </div>
